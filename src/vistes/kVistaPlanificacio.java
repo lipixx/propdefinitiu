5e0ff269fdf8cda2dcb24df1802001d7a3d2b1a9
lipixx <lipixx@987b935c-474e-0410-a20b-1394c66f8fc1>
2008-06-06 03:21:19 +0000

    mod crit

--- src/vistes/kVistaPlanificacio.java
+++ src/vistes/kVistaPlanificacio.java
@@ -159,7 +159,7 @@ public class kVistaPlanificacio {
                                 vPlani.setSelectPlan(planSelected);
                             }
                         } else {
-                            JOptionPane.showMessageDialog(null, "Aquesta emissio ja ha estat emesa o facturada.");
+                            JOptionPane.showMessageDialog(null, "Aquesta emissio ja ha estat emesa, facturada, o queden menys de 24h per ser vista.");
                         }
 
                     }
